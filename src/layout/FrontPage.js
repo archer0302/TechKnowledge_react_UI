d4f37bbb7775cf4a41cb966ecedcf22a0f684c5d
Archer Chang <acha0128@student.monash.edu>
2021-04-27 14:57:43 +1000

    add search button

--- src/layout/FrontPage.js
+++ src/layout/FrontPage.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
-import Autocomplete from '@material-ui/lab/Autocomplete';
+import Button from '@material-ui/core/Button';
+import Autocomplete, { createFilterOptions } from '@material-ui/lab/Autocomplete';
 import tag_list from '../data/tag_full_list.json';
 
 export default function FrontPage() {
+
+  const filterOptions = createFilterOptions({
+    matchFrom: 'any',
+    limit: 10,
+  });
+
   return (
-    <div style={{ width: 300 }}>
-      <Autocomplete
-        id="free-solo-demo"
-        freeSolo
-        options={tag_list}
-        renderInput={(params) => (
-          <TextField {...params} label="freeSolo" margin="normal" variant="outlined" />
-        )}
-      />
+    <div style={{display:'flex', flexDirection: 'row', alignItems: 'center'}}>
+      <div>
+        <Autocomplete
+          style={{width: 500}}
+          id="free-solo-demo"
+          freeSolo
+          filterOptions={filterOptions}
+          options={tag_list}
+          size="small"
+          renderInput={(params) => (
+            <TextField {...params} label="freeSolo" margin="normal" variant="outlined" />
+          )}
+        />
+      </div>
+      <div style={{marginLeft: '10px'}}>
+        <Button variant="contained" color="primary" size="medium">
+          Search
+        </Button>
+      </div>
     </div>
   )
 }
