0a51ef90d348b5b051636bc302729019dd3b9d80
Archer Chang <archer.chang32@gmail.com>
2021-06-12 00:46:49 +1000

    fix constructor keyword issue

--- src/data/db/FaunadbExample.js
+++ src/data/db/FaunadbExample.js
@@ -12,27 +12,27 @@ var serverClient = new faunadb.Client({ secret: 'fnAEJE0jifACCDBqxs7jFi7KX_5o5l8
 
 serverClient.query(
   q.Get(
-    q.Match(q.Index('trend_get_by_tag'), 'java')
+    q.Match(q.Index('get_links_by_center'), 'tostring')
   )
 )
-.then((ret) => console.log(ret))
+.then((ret) => console.log(ret.data.links))
 .catch((err) => console.error('Error: %s', err))
 
 serverClient.query(
   q.Get(
-    q.Match(q.Index('link_get_by_center'), 'html')
+    q.Match(q.Index('get_nodes_by_center'), 'tostring')
   )
 )
 .then((ret) => console.log(ret.data))
 .catch((err) => console.error('Error: %s', err))
 
-serverClient.query(
-  q.Get(
-    q.Match(q.Index('node_get_by_center'), 'html')
-  )
-)
-.then((ret) => console.log(ret))
-.catch((err) => console.error('Error: %s', err))
+// serverClient.query(
+//   q.Get(
+//     q.Match(q.Index('node_get_by_center'), 'html')
+//   )
+// )
+// .then((ret) => console.log(ret))
+// .catch((err) => console.error('Error: %s', err))
 
 // Search by index
 // serverClient.query(
